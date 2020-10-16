c470151db88d5f8f9ccfb612433cb62ef94c02ae
rulemur <rulemur@gmail.com>
2020-10-16 21:21:06 +0300

    init lesson1

---
+++ VKotBOGA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKotBOGA
+//
+//  Created by Александр Павлов on 13.09.2020.
+//  Copyright © 2020 Александр Павлов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
